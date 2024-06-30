af79386abe0857fe1c30be97eec760dbd84022c5
Chlumsky <viktor.chlumsky@gmail.com>
2024-06-30 17:26:56 +0200

    Version 1.1

--- artery-font/artery-font.h
+++ artery-font/artery-font.h
@@ -6,5 +6,5 @@
 #include "structures.h"
 #include "serialization.h"
 
-// ARTERY ENGINE ATLAS FONT FORMAT LIBRARY v1.0.1
-// Author: Viktor Chlumsky (c) 2020 - 2022
+// ARTERY ENGINE ATLAS FONT FORMAT LIBRARY v1.1
+// Author: Viktor Chlumsky (c) 2020 - 2024
